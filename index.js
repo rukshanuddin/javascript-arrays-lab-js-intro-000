c4b4f890fac1ce507058afd5a8fd80a8cecc41b0
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-18 01:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name){
   return kittens.push(name)
 }// Add your functions and code here
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
