fb3c4279c08bc68ab15123855826b0ca9b7d9e64
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-18 01:57:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ function appendKitten(name){
 function prependKitten(name){
   return [name, ...kittens]
 }
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
 function removeLastKitten(){
-  return kittens.slice(-1)
+  return kittens.slice(0, kittens.length - 1)
 }
